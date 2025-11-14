124404f81cc9b3ac690fc6230faa736a3b6830ac
14247011 <14247011@205-21.Fortis.local>
2025-11-14 12:22:13 +0100

    Bereich erstellt und Test für Main

---
+++ src/zoo/Bereich.java
@@ -0,0 +1,20 @@
+package zoo;
+/**
+ *
+ * @author 14247011
+ */
+public class Bereich {
+String name;
+double preis;
+ public Bereich(String name, double preis) {
+     this.name = name;
+     this.preis = preis;
+ }
+ public String getName() {
+     return name;
+ }
+ public double getPreis() {
+ return preis;
+  }
+ }
+
--- src/zoo/main.java
+++ src/zoo/main.java
@@ -1,8 +1,25 @@
 package zoo;
 
+import java.util.ArrayList;
+import java.util.List; 
+
 public class main {
     public static void main(String[] args) {
-    Person person = new Person("Lenny", 12);
-    Angebot angebot = new Angebot("Test", 2.0);
+    
+        List<Person> gruppe = new ArrayList<>();
+        gruppe.add(new Person("Lenny", 12));
+        gruppe.add(new Person("Darian", 70));
+        gruppe.add(new Person("Sonne", 42));
+        
+        List<Angebot> angebote = new ArrayList<>();
+        angebote.add(new Angebot("Hirsche streicheln", 100.50));
+        angebote.add(new Angebot("Bären-Show", 12.00));
+        
+        Parkticket park = new Parkticket(5.00);
+        
+        List<Bereich> bereich = List.of(
+        new Bereich("Eurasien", 3.00),
+        new Bereich("Nordamerika", 2.50)
+        );
  }
 }
