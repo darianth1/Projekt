d8a9eb27757288dde5f35fc2156f6cc29925ee1b
14247011 <14247011@205-21.Fortis.local>
2025-11-14 11:46:48 +0100

    Parkticket Klasse

---
+++ src/zoo/Parkticket.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zoo;
+
+/**
+ *
+ * @author 14247011
+ */
+public class Parkticket {
+    public double ticket;
+    
+    public Parkticket(double preis){
+      this.ticket = ticket;  
+    }
+    public double getPreis() {
+        return ticket;
+    }
+    }
